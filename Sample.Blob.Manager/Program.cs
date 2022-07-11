b6f559338ea67dc462c95aef2057ba3da6e5bc8b
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-07-11 17:16:10 -0300

    feat: adicionar string de conexão no Program.cs

--- Sample.Blob.Manager/Program.cs
+++ Sample.Blob.Manager/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Storage.Blobs;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped(options =>
+{
+    return new BlobServiceClient(builder.Configuration.GetConnectionString("AzureStorage"));
+});
 
 var app = builder.Build();
 
