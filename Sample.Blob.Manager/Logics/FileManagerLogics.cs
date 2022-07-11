8c8cd87831d9fb0c5d72055c36647ed12e866973
Thomas Ravache dos Santos <thomas.santos@conveste.com.br>
2022-07-11 18:38:14 -0300

    refactor: aprimorar logica do blobclient

--- Sample.Blob.Manager/Logics/FileManagerLogics.cs
+++ Sample.Blob.Manager/Logics/FileManagerLogics.cs
@@ -12,11 +12,17 @@ namespace Sample.Blob.Manager.Logics
             _blobServiceClient = blobServiceClient;
         }
 
+        public BlobClient GetBlobClient(string containerName, string fileName)
+        {
+            var blobContainer = _blobServiceClient.GetBlobContainerClient(containerName);
+
+             return blobContainer.GetBlobClient(fileName); // é bom colocar o timestamp no final do arquivo
+        }
+
         public async Task Upload(FileModel model)
         {
-            var blobContainer = _blobServiceClient.GetBlobContainerClient("upload-file");
+            var blobClient = GetBlobClient("upload-file", model.MyFile.FileName); // é bom colocar o timestamp no final do arquivo
 
-            var blobClient = blobContainer.GetBlobClient(model.MyFile.FileName); // é bom colocar o timestamp no final do arquivo
             await blobClient.UploadAsync(model.MyFile.OpenReadStream());
 
             // se não quiser que o arquivo com o mesmo nome seja sobrescrito utilizar desta forma abaixo:
@@ -25,9 +31,7 @@ namespace Sample.Blob.Manager.Logics
 
         public async Task<byte[]> Read(string filename)
         {
-            var blobContainer = _blobServiceClient.GetBlobContainerClient("upload-file");
-
-            var blobClient = blobContainer.GetBlobClient(filename); // é bom colocar o timestamp no final do arquivo
+            var blobClient = GetBlobClient("upload-file", filename);
 
             var fileDownloaded = await blobClient.DownloadAsync();
 
